7e45cb3eb5e379e3bc7a97faa0d3d4304e6aaeb3
Chaggar25 <142560645+Chaggar25@users.noreply.github.com>
2023-08-28 02:26:40 -0400

    <we learned how to do for loops>

---
+++ script.js
@@ -0,0 +1,21 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.lenth; x++) {
+    
+    console.log(topics[x]);
+}
+
+
+/*
+if (topic === 'HTML') {
+    console.log("Let Study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Lets Study CSS!");
+} else if (topic === 'Git') {
+    console.log("Lets Study Git!");
+} else if (topic === 'JavaScript') {
+    console.log('Lets Study JavaScript!');
+} else {
+    console.log('please try again!');
+}
+*/
